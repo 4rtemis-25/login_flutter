dab75804a653b9687f7e722b68b5007d2c5f2698
4rtemis-25 <121249378+4rtemis-25@users.noreply.github.com>
2023-11-10 17:44:52 -0500

    login

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:login/widgets/background.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return const Scaffold(
+      body: Background(),
+    );
   }
 }
